75a496df9dfd1f680005da14fdd5995b10ebc288
Chakrapani <chitranshsharma53@gmail.com>
2024-04-08 21:18:49 +0530

    navbar completed

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useCallback } from "react";
+import  { useEffect,useState, useCallback } from "react";
 import logo from "../assets/images/logo.png";
 import NavbarItems from "./NavbarItems";
 import { FiChevronDown } from "react-icons/fi";
 import { BsSearch, BsBell } from "react-icons/bs";
 import MobileMenu from "./MobileMenu";
 import profileimg from "../assets/images/default-blue.png"
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 60;
 
 const Navbar = () => {
   const NavItems = [
@@ -16,14 +19,37 @@ const Navbar = () => {
     "Browse by Language",
   ];
   const [showMobileMenu, setshowMobileMenu] = useState(false);
+  const [showAccountMenu, setshowAccountMenu] = useState(false);
+  const [showBackground , setshowBackground] = useState(false);
 
   const toggleMobileMenu = useCallback(() => {
     setshowMobileMenu((current) => !current);
   }, []);
+  const togglAccountMenu = useCallback(() => {
+    setshowAccountMenu((current) => !current);
+  }, []);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY)
+      if (window.scrollY >= TOP_OFFSET) {
+        setshowBackground(true)
+      } else {
+        setshowBackground(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
   return (
     <>
       <nav className="fixed w-full z-50">
-        <div className="flex flex-row items-center px-4 py-6 md:px-14 bg-zinc-900 bg-opacity-90 transition duration-500">
+        <div className={`flex flex-row items-center px-4 py-6 md:px-14 transition duration-500  ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
           <img src={logo} alt="logo" className="h-4 lg:h-8"></img>
           <div className="flex-row gap-6 hidden lg:flex ml-8">
             {NavItems.map((item, index) => (
@@ -35,21 +61,24 @@ const Navbar = () => {
             className="lg:hidden flex flex-row gap-2 ml-8 cursor-pointer relative items-center"
           >
             <p className="text-white text-sm">Browse</p>
-            <FiChevronDown className="text-white transition" />
+            <FiChevronDown className={`text-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`}/>
             <MobileMenu visible={showMobileMenu} />
           </div>
 
-          <div className="flex flex-row ml-auto gap-7 cursor-pointer">
-            <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+          <div className="flex flex-row ml-auto gap-7 cursor-pointer items-center">
+            <div className="text-gray-200 hover:text-gray-400 cursor-pointer transition">
               <BsSearch />
             </div>
-            <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <div className="text-gray-200 hover:text-gray-400 cursor-pointer transition">
               <BsBell />
             </div>
-            <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+
+            <div onClick={togglAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
               <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md cursor-pointer overflow-hidden">
                 <img src={profileimg} alt="profile image"></img>
               </div>
+              <FiChevronDown className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
+              <AccountMenu visible={showAccountMenu}/>
             </div>
           </div>
         </div>
