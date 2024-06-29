f96c2f4346d98a87d2a0378759fbfe8359977acb
Chakrapani <chitranshsharma53@gmail.com>
2024-06-29 17:40:39 +0530

    made some changes in profile component

--- Client/src/pages/profile/Profile.jsx
+++ Client/src/pages/profile/Profile.jsx
@@ -31,7 +31,7 @@ const Profile = ({user}) => {
                   alt="...loading"
                 />
               </div>
-              <h2 className='text-center text-white mt-2 text-2xl'>{user.username}</h2>
+              <h2 className='text-center text-white mt-2 text-2xl'>{user?.username}</h2>
             </div>
           </div>
         </div>
