29a177777417ba7e30dec9e7a7d34647f383e95b
Chakrapani <chitranshsharma53@gmail.com>
2024-06-29 23:12:18 +0530

    all working perfectly

--- Client/src/components/AccountMenu.jsx
+++ Client/src/components/AccountMenu.jsx
@@ -1,10 +1,11 @@
 import profileimg from "../assets/images/default-blue.png";
-
+import useUserInfo from '../hook/useUserInfo';
 import {  useNavigate } from "react-router";
 
-const AccountMenu = ({visible}) => {
-  const userName = localStorage.getItem("username")
-  console.log(userName)
+const AccountMenu = ({visible}) => { 
+  
+  const {user} = useUserInfo();
+ 
   const navigate = useNavigate();
   if(!visible){
     return null;
@@ -18,7 +19,7 @@ const AccountMenu = ({visible}) => {
     <div className="flex flex-col gap-3">
       <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
         <img className="w-8 rounded-md" src={profileimg} alt="" />
-        <p className="text-white text-sm group-hover/item:underline">{userName}</p>
+        <p className="text-white text-sm group-hover/item:underline">{user?.userName}</p>
       </div>
     </div>
     <hr className="bg-gray-600 border-0 h-px my-4" />
